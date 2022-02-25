1426313c7adb588855865dc461149e04b5c33d10
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-02-25 23:32:30 +0900

    7강 배열

---
+++ src/main/java/lec02/class7.java
@@ -0,0 +1,30 @@
+package lec02;
+
+public class class7 {
+    //7강 배열
+    public static void main(String[] args) {
+        int[] arr1 = new int[5];
+        arr1[0] = 100;
+        arr1[1] = 200;
+        arr1[2] = 300;
+        arr1[3] = 400;
+        arr1[4] = 500;
+        System.out.println("arr1[0] : "+arr1[0]);
+        System.out.println("arr1[1] : "+arr1[1]);
+        System.out.println("arr1[2] : "+arr1[2]);
+        System.out.println();
+
+        /*
+        int[] arr = new int[3]
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+         */
+        int[] arr = {10,20,30};
+        System.out.println("arr[0] : "+arr[0]);
+        System.out.println("arr[1] : "+arr[1]);
+        System.out.println("arr[2] : "+arr[2]);
+    }
+
+}
