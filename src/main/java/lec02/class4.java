eb1df1d59065af14c1526fbc3a93c46569d2dd13
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-02-25 17:39:36 +0900

    4강 기본자료형

---
+++ src/main/java/lec02/class4.java
@@ -0,0 +1,25 @@
+package lec02;
+
+public class class4 {
+    //4강 기본자료형
+    public static void main(String[] args) {
+
+        char c = 'a';
+        System.out.println("c = "+c);
+
+        int i = 10;
+        System.out.println("i = "+i);
+
+        double d = 10.259;
+        System.out.println("d = "+d);
+
+        boolean b = false;
+        System.out.println("b = "+b);
+
+        String s = "Hello Java World";
+        System.out.println("s = "+s);
+
+
+    }
+
+}
