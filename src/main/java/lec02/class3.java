917293b996d478dcdcf758e65f5fa441261bf470
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-02-25 17:02:56 +0900

    3강 변수

---
+++ src/main/java/lec02/class3.java
@@ -0,0 +1,21 @@
+package lec02;
+// 3강_변수
+public class class3 {
+    public static void main(String[] args) {
+        String i; // 변수 선언
+        i = "10"; //변수 초기화
+
+        int j = 20; //변수 선언 초기화 동시에
+        int k = 10;
+        System.out.println("i+j = "+(i+j));
+        System.out.println("j+k = "+(j+k));
+
+        int num = 0;
+        System.out.println("num = "+num);
+
+        num = 100;
+        System.out.println("num = "+num);
+
+
+    }
+}
